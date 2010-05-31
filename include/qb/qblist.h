6b052988a9d38ffa59187d6c1dbea4f96c73ce83
Angus Salkeld <asalkeld@redhat.com>
2010-05-31 15:22:12 +1000

    man: document qblist.h better.
    Signed-off-by: Angus Salkeld <asalkeld@redhat.com>

--- include/qb/qblist.h
+++ include/qb/qblist.h
@@ -25,20 +25,42 @@
 extern "C" {
 #endif
 
+/**
+ * @file qblist.h
+ * @author Steven Dake <sdake@redhat.com>
+ *
+ * This is a kernel style list implementation.
+ */
+
+
 struct qb_list_head {
 	struct qb_list_head *next;
 	struct qb_list_head *prev;
 };
 
+/**
+ * @def QB_DECLARE_LIST_INIT()
+ * Declare and initialize a list head.
+ */
 #define QB_DECLARE_LIST_INIT(name) \
     struct qb_list_head name = { &(name), &(name) }
 
+/**
+ * Initialize the list entry.
+ * Points next and prev pointers to head.
+ */
 static void inline qb_list_init (struct qb_list_head *head)
 {
 	head->next = head;
 	head->prev = head;
 }
 
+/**
+ * Add this element to the list.
+ *
+ * @param element the new element to insert.
+ * @param head the list head to add to.
+ */
 static void inline qb_list_add (struct qb_list_head *element, struct qb_list_head *head)
 {
 	head->next->prev = element;
@@ -46,6 +68,11 @@ static void inline qb_list_add (struct qb_list_head *element, struct qb_list_hea
 	element->prev = head;
 	head->next = element;
 }
+
+/**
+ * Add to the list (but at the end of the list).
+ * @see qb_list_add()
+ */
 static void inline qb_list_add_tail (struct qb_list_head *element, struct qb_list_head *head)
 {
 	head->prev->next = element;
@@ -53,6 +80,28 @@ static void inline qb_list_add_tail (struct qb_list_head *element, struct qb_lis
 	element->prev = head->prev;
 	head->prev = element;
 }
+
+/**
+ * Delete an entry from the list.
+ *
+ * The code below shows howto delete an entry safely from within a list.
+ * @code
+ *	struct my_struct *mine;
+ *	struct list_head *iter, *iter_next;
+ *
+ *	for (iter = my_list_head.next;
+ *		iter != &my_list_head;
+ *		iter = iter_next) {
+ *
+ *		iter_next = iter->next;
+ *
+ *		mine = qb_list_entry(iter, struct my_struct, list);
+ *		qb_list_del (&mine->list);
+ *		free (mine);
+ *	}
+ *
+ * @endcode
+ */
 static void inline qb_list_del (struct qb_list_head *_remove)
 {
 	_remove->next->prev = _remove->prev;
@@ -63,9 +112,33 @@ static void inline qb_list_del (struct qb_list_head *_remove)
 #endif
 }
 
+/**
+ * @def qb_list_entry(ptr,type,member)
+ * Get the user data from the list entry.
+ *
+ * The code below shows how to use qb_list_entry() from within a loop.
+ * @code
+ *	struct my_struct *mine = NULL;
+ *	struct qb_list_head *iter;
+ *
+ *	for (iter = my_list_head.next;
+ *		iter != &my_list_head;
+ *		iter = iter->next) {
+ *		mine = qb_list_entry(iter, struct my_struct, list);
+ *		if (mine == NULL) {
+ *			continue;
+ *		}
+ *		do_some_thing (mine);
+ *	}
+ *
+ * @endcode
+ */
 #define qb_list_entry(ptr,type,member)\
 	((type *)((char *)(ptr)-(unsigned long)(&((type *)0)->member)))
 
+/**
+ * A quick test to see if the list is empty (pointing to it's self).
+ */
 static inline int qb_list_empty(const struct qb_list_head *l)
 {
 	return l->next == l;
