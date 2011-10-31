b40fa6a87478e6a6572bb68bf2ad9d8fa334f1ce
Angus Salkeld <asalkeld@redhat.com>
2011-10-31 11:46:35 +1100

    LOG: use OutputDebugStringA() on windows instead of syslog
    Signed-off-by: Angus Salkeld <asalkeld@redhat.com>

--- lib/log_syslog.c
+++ lib/log_syslog.c
@@ -19,61 +19,74 @@
  * along with libqb.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "os_base.h"
+
 #ifdef HAVE_SYSLOG_H
 #include <syslog.h>
 #endif /* HAVE_SYSLOG_H */
 #include "log_int.h"
 
+#ifdef QB_MINGW
+static void
+_win_debugger(int32_t target,
+	      struct qb_log_callsite *cs, time_t timestamp, const char *msg)
+{
+	char output_buffer[QB_LOG_MAX_LEN];
+	struct qb_log_target *t = qb_log_target_get(target);
+
+	qb_log_target_format(target, cs, timestamp, msg, output_buffer);
+
+	OutputDebugStringA(output_buffer);
+}
+
+#else
+
 static void
 _syslog_logger(int32_t target,
 	       struct qb_log_callsite *cs, time_t timestamp, const char *msg)
 {
-#ifdef HAVE_SYSLOG_H
 	char output_buffer[QB_LOG_MAX_LEN];
-#endif /* HAVE_SYSLOG_H */
 	struct qb_log_target *t = qb_log_target_get(target);
 	int32_t final_priority = cs->priority + t->priority_bump;
 
 	if (final_priority > LOG_DEBUG) {
 		return;
 	}
-#ifdef HAVE_SYSLOG_H
 	qb_log_target_format(target, cs, timestamp, msg, output_buffer);
 
 	if (final_priority < LOG_EMERG) {
 		final_priority = LOG_EMERG;
 	}
 	syslog(final_priority, "%s", output_buffer);
-#endif /* HAVE_SYSLOG_H */
 }
 
 static void
 _syslog_close(int32_t target)
 {
-#ifdef HAVE_SYSLOG_H
 	closelog();
-#endif /* HAVE_SYSLOG_H */
 }
 
 static void
 _syslog_reload(int32_t target)
 {
-#ifdef HAVE_SYSLOG_H
 	struct qb_log_target *t = qb_log_target_get(target);
 	closelog();
 	openlog(t->name, LOG_PID, t->facility);
-#endif /* HAVE_SYSLOG_H */
 }
+#endif
 
 int32_t
 qb_log_syslog_open(struct qb_log_target *t)
 {
+#ifdef QB_MINGW
+	t->logger = _win_debugger;
+	t->reload = NULL;
+	t->close = NULL;
+#else
 	t->logger = _syslog_logger;
 	t->reload = _syslog_reload;
 	t->close = _syslog_close;
 
-#ifdef HAVE_SYSLOG_H
 	openlog(t->name, LOG_PID, t->facility);
-#endif /* HAVE_SYSLOG_H */
+#endif
 	return 0;
 }
