bb2e307241d0e06a49bbd125df9c34dfdae1d24c
Andrew Beekhof <andrew@beekhof.net>
2012-06-07 14:41:03 +1000

    LOG: Gracefully fail when the caller exceeds QB_LOG_MAX_LEN

--- lib/log_blackbox.c
+++ lib/log_blackbox.c
@@ -96,6 +96,13 @@ _blackbox_vlogger(int32_t target,
 
 	/* log message */
 	msg_len = qb_vsprintf_serialize(chunk, cs->format, ap);
+	if(msg_len > QB_LOG_MAX_LEN) {
+	    chunk = msg_len_pt + sizeof(uint32_t); /* Reset */
+
+	    msg_len = qb_vsprintf_serialize(chunk, "Log message too long to be stored in the blackbox.  Maximum is QB_LOG_MAX_LEN" , ap);
+	    actual_size += msg_len;
+	}
+
 	actual_size += msg_len;
 
 	/* now that we know the length, write it
