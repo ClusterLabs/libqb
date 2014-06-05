edd2eec16c46b3970825705e6b695b73b936165c
Michael Chapman <mike@very.puzzling.org>
2014-06-05 14:29:28 +1000

    ringbuffer: fix qb_rb_open_2() debug format string
    qb_log_target_formats() does not support formatting size_t values with
    %zd.  Use %ld to format them as long integers instead.

--- lib/ringbuffer.c
+++ lib/ringbuffer.c
@@ -232,7 +232,7 @@ qb_rb_open_2(const char *name, size_t size, uint32_t flags,
 	}
 
 	qb_util_log(LOG_DEBUG,
-		    "shm size:%zd; real_size:%zd; rb->word_size:%d", size,
+		    "shm size:%ld; real_size:%ld; rb->word_size:%d", size,
 		    real_size, rb->shared_hdr->word_size);
 
 	/* this function closes fd_data */
