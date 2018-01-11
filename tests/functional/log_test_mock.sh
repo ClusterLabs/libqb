5ee38db1d8dc71d72b1215c75e12bc86d5b47902
Jan Pokorný <jpokorny@redhat.com>
2018-01-11 17:42:21 +0100

    tests: log_test_mock.sh: allow for V=1 propagation to partial make's
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- tests/functional/log_test_mock.sh
+++ tests/functional/log_test_mock.sh
@@ -202,6 +202,7 @@ do_proceed () {
 	while :; do
 		case "$1" in
 		shell) shift; do_shell "$@"; return;;
+		-v)    _makevars="${_makevars} V=1"; shift;;
 		-nsc)  case "${_resultsdir_tag}" in
 		       *sc*) do_die "do not combine \"sc\" flags";; esac
 		       _resultsdir_tag="${_resultsdir_tag}$1"; shift;
@@ -220,7 +221,7 @@ do_proceed () {
 	done
 
 	if test -n "${_resultsdir_tag}"; then
-		_makevars="CPPFLAGS=\" \
+		_makevars="${_makevars} CPPFLAGS=\" \
 		           $(test "${_clientselfcheck}" -eq 1 || printf %s ' -DNSELFCHECK') \
 		           $(test "${_interlibselfcheck}" -eq 1 || printf %s ' -DNLIBSELFCHECK') \
 		           $(test "${_clientlogging}" -eq 1 || printf %s ' -DNLOG') \
@@ -333,8 +334,9 @@ do_proceed () {
 }
 
 { test $# -eq 0 || test "$1" = -h || test "$1" = --help; } \
-  && printf '%s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n' \
-            "usage: $0 {[-n{{,c,i}sc,cl,il}]* <libqb.src.rpm> | shell}" \
+  && printf '%s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n' \
+            "usage: $0 {[-{v,n{{,c,i}sc,cl,il}}]* <libqb.src.rpm> | shell}" \
+            "- use '-v' to show the compilation steps verbosely" \
             "- use '-nsc' to suppress self-check (\"see whole story\") wholly" \
             "- use '-ncsc' to suppress self-check client-side only" \
             "- use '-nisc' to suppress self-check interlib-side only" \
