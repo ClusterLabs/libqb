e6c7798faec96f0e765cf9f6944f36da60f7246a
Fabio M. Di Nitto <fdinitto@redhat.com>
2019-03-15 10:01:40 +0100

    [tests] first pass at fixing test execution
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- tests/blackbox-segfault.sh
+++ tests/blackbox-segfault.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 #
 # create a normal blackbox
+
+# use in-tree qb-blackbox when available
+if [ -f ../tools/qb-blackbox ]; then
+ QBBLACKBOX=../tools/qb-blackbox
+else
+ QBBLACKBOX=qb-blackbox
+fi
+
+#
+# create a normal blackbox
+#
 rm -f crash-test-dummy.fdata
 ./crash_test_dummy
 rm -f core*
@@ -9,18 +20,17 @@ rm -f core*
 
 # first test that reading the valid
 # blackbox data actually works.
-../tools/qb-blackbox crash-test-dummy.fdata
+$QBBLACKBOX crash-test-dummy.fdata
 if [ $? -ne 0 ]; then
 	exit 1
 fi
-
 
 for i in $(seq $NUM_BB_TESTS)
 do
     rm -f butchered_blackbox.fdata
     echo " ==== Corrupt blackbox test $i/$NUM_BB_TESTS ===="
     ./file_change_bytes -i crash-test-dummy.fdata -o butchered_blackbox.fdata -n 1024
-    ../tools/qb-blackbox butchered_blackbox.fdata
+    $QBBLACKBOX butchered_blackbox.fdata
     [ $? -gt 127 ] && exit 1 || true
 done
 
