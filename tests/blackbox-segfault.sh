a0a0e1a58c47f5f2f750c899c499717d106146c5
Christine Caulfield <ccaulfie@redhat.com>
2019-03-18 11:29:06 +0000

    tests: allow blackbox-segfault.sh to run out-of-tree

--- tests/blackbox-segfault.sh
+++ tests/blackbox-segfault.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
 #
+# Needs PATH to be set to find accompanying test programs
+# - including qb-blackbox which for in-tree tests should be
+# - in ../tools
+#
 # create a normal blackbox
-
-# use in-tree qb-blackbox when available
-if [ -f ../tools/qb-blackbox ]; then
- QBBLACKBOX=../tools/qb-blackbox
-else
- QBBLACKBOX=qb-blackbox
-fi
+#
 
 #
 # create a normal blackbox
 #
 rm -f crash-test-dummy.fdata
-./crash_test_dummy
+crash_test_dummy
 rm -f core*
 
-. ./test.conf
+. test.conf
 
 # first test that reading the valid
 # blackbox data actually works.
-$QBBLACKBOX crash-test-dummy.fdata
+qb-blackbox crash-test-dummy.fdata
 if [ $? -ne 0 ]; then
 	exit 1
 fi
@@ -29,8 +27,8 @@ for i in $(seq $NUM_BB_TESTS)
 do
     rm -f butchered_blackbox.fdata
     echo " ==== Corrupt blackbox test $i/$NUM_BB_TESTS ===="
-    ./file_change_bytes -i crash-test-dummy.fdata -o butchered_blackbox.fdata -n 1024
-    $QBBLACKBOX butchered_blackbox.fdata
+    file_change_bytes -i crash-test-dummy.fdata -o butchered_blackbox.fdata -n 1024
+    qb-blackbox butchered_blackbox.fdata
     [ $? -gt 127 ] && exit 1 || true
 done
 
