842053c1743c1a73d891c4c31211cfa08b201717
Christine Caulfield <ccaulfie@redhat.com>
2019-06-19 15:38:40 +0100

    ipc: fix force-filesystem-sockets
    the /etc/libqb/force-filesystem-sockets option got broken for some
    applications in the last security update.

--- lib/ipc_socket.c
+++ lib/ipc_socket.c
@@ -71,7 +71,7 @@ set_sock_addr(struct sockaddr_un *address, const char *socket_name)
 	address->sun_len = QB_SUN_LEN(address);
 #endif
 
-	if (!use_filesystem_sockets()) {
+	if (socket_name[0] == '/' || !use_filesystem_sockets()) {
 		snprintf(address->sun_path + 1, UNIX_PATH_MAX - 1, "%s", socket_name);
 	} else {
 		snprintf(address->sun_path, sizeof(address->sun_path), "%s/%s", SOCKETDIR,
