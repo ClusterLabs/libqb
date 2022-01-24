76828228ee81eedb91296b526d85c923a7e9786e
Christine Caulfield <ccaulfie@redhat.com>
2022-01-24 13:03:41 +0000

    Catch stat() as well as __xstat()
    F35 and Centos9 seem to have reverted back to an actual stat()
    call rather than a wrapper around __xstat(), so trap both just
    in case.
    
    Other small fixes

--- tests/libstat_wrapper.c
+++ tests/libstat_wrapper.c
@@ -11,6 +11,7 @@
 #include <gnu/lib-names.h>
 #endif
 
+// __xstat for ealier libc
 int __xstat(int __ver, const char *__filename, struct stat *__stat_buf)
 {
 #if defined(QB_LINUX) || defined(QB_CYGWIN)
@@ -25,10 +26,36 @@ int __xstat(int __ver, const char *__filename, struct stat *__stat_buf)
 	}
 
 	if (strcmp(__filename, FORCESOCKETSFILE) == 0) {
+		fprintf(stderr, "__xstat called for %s\n", __filename);
 		return 0; /* it exists! */
 	}
 
 	return real_xstat(__ver, __filename, __stat_buf);
+#else
+	return -1; /* Error in the unlikely event we get called on *BSD* */
+#endif
+}
+
+// Stat for F35 and later
+int stat(const char *__filename, struct stat *__stat_buf)
+{
+#if defined(QB_LINUX) || defined(QB_CYGWIN)
+	static int opened = 0;
+	static void *dlhandle;
+	static int (*real_stat)(const char *__filename, void *__stat_buf);
+
+	if (!opened) {
+		dlhandle = dlopen(LIBC_SO, RTLD_NOW);
+		real_stat = dlsym(dlhandle, "stat");
+		opened = 1;
+	}
+
+	if (strcmp(__filename, FORCESOCKETSFILE) == 0) {
+		fprintf(stderr, "stat called for %s\n", __filename);
+		return 0; /* it exists! */
+	}
+
+	return real_stat(__filename, __stat_buf);
 #else
 	return -1; /* Error in the unlikely event we get called on *BSD* */
 #endif
